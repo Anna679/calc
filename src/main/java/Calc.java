c42c576bf318e19de8d579a6c9adaa437bb6d10c
anna679 <anna679kuchaeva@gmail.com>
2017-12-11 19:30:26 +0300

    1

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -8,14 +8,76 @@ public class Calc {
      @param args */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        float x = scanner.nextFloat();
-        System.out.printf ("%.4f\n ",x);
-        System.out.print("Введите второе число: ");
-        float y = scanner.nextFloat();
-        System.out.printf ("%.4f\n ",y);
-        System.out.print("Сумма: ");
-        System.out.println(x+y);
-    }
-}
+        Integer z = scanner.nextInt();
+        if ( z == 1 ){
+            System.out.println("Выбран поиск максимального слова в массиве");
+            System.out.println("Введе размерность массива ");
+            Scanner in = new Scanner(System.in);
+            int size = in.nextInt();
+            String[] Massiv = new String[size] ;
+            System.out.println("Введите элементы массива,т.е слова");
+            String bigWord = "";// переменная для хранения слова с наибольшим числом символов
 
+            for (int i = 0; i < Massiv.length; i++) {
+                String B = scanner.next();
+                Massiv[i] = B;
+                String A = Massiv[i];
+                System.out.println(A);
+                System.out.println(A.length() );
+                if (Massiv[i].length() > bigWord.length()) {
+                    bigWord = Massiv[i];
+
+
+                }
+            }
+            System.out.println("Самое большое слово в массиве" + bigWord);
+        }
+        else  if ( z == 2 ) {
+            System.out.println("Выбран калькулятор");
+            System.out.println("Введите оператор ");
+            String operator;
+            operator = scanner.next();
+                  if(operator.equals("+")){
+                    System.out.println("Выбран оператор сложения");
+                    System.out.print("Введите первое число: ");
+                    float x = scanner.nextFloat();
+                    System.out.printf ("%.4f\n ",x);
+                    System.out.print("Введите второе число: ");
+                    float y = scanner.nextFloat();
+                    System.out.printf ("%.4f\n ",y);
+                    System.out.print("Сумма: ");
+                    System.out.println(x+y);
+                  }if(operator.equals("-")){
+                     System.out.println("Выбран оператор вычитания");
+                     System.out.print("Введите первое число: ");
+                     float x = scanner.nextFloat();
+                     System.out.printf ("%.4f\n ",x);
+                     System.out.print("Введите второе число: ");
+                     float y = scanner.nextFloat();
+                     System.out.printf ("%.4f\n ",y);
+                     System.out.print("Сумма: ");
+                     System.out.println(x-y);
+                 }if(operator.equals("*")){
+                    System.out.println("Выбран оператор умножения");
+                    System.out.print("Введите первое число: ");
+                    float x = scanner.nextFloat();
+                    System.out.printf ("%.4f\n ",x);
+                    System.out.print("Введите второе число: ");
+                    float y = scanner.nextFloat();
+                    System.out.printf ("%.4f\n ",y);
+                    System.out.print("Сумма: ");
+                    System.out.println(x*y);
+                }if (operator.equals("/")){
+                    System.out.println("Выбран оператор деления");
+                    System.out.print("Введите первое число: ");
+                    float x = scanner.nextFloat();
+                    System.out.printf ("%.4f\n ",x);
+                    System.out.print("Введите второе число: ");
+                    float y = scanner.nextFloat();
+                    System.out.printf ("%.4f\n ",y);
+                    System.out.print("Сумма: ");
+                    System.out.println(x/y);
+                }
+            }
+        }
+    }
